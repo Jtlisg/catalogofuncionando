008b776d952e0947243cae660320c97f67e671c9
Jonatan Lisama <jtlisama21@gmail.com>
2025-11-12 23:06:45 -0600

    config correcion key

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,6 @@
-export const SUPABASE_URL = "https://gerithiadfexpsbghluq.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh...";
-export const ADMIN_KEY = "admin123";
 
+
+// config.js
+export const SUPABASE_URL = "https://gerithiadfexpsbghluq.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlcml0aGlhZGZleHBzYmdobHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODUxMTEsImV4cCI6MjA3ODU2MTExMX0.7dJRBo94-32yXZb6rEFkmAY9YEloBXxujK1eowG9InQ";
+export const ADMIN_KEY = "admin123";
